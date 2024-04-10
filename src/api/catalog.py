767dd312fe55f99b374b95bd7ab53970affdb2ee
eswenke <swenke.ethan.us@gmail.com>
2024-04-10 13:38:43 -0700

    fixed naive approach

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -21,7 +21,7 @@ def get_catalog():
             {
                 "sku": "GREEN_POTION_0",
                 "name": "green potion",
-                "quantity": result,
+                "quantity": 1,
                 "price": 50,
                 "potion_type": [0, 100, 0, 0],
             }
