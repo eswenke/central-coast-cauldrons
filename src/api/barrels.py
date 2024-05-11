4badda598d96c63c464665ab6217b95a2486d17b
eswenke <swenke.ethan.us@gmail.com>
2024-05-11 01:20:38 -0700

    forcing dark barrel purchase

--- src/api/barrels.py
+++ src/api/barrels.py
@@ -238,7 +238,7 @@ def get_wholesale_purchase_plan(wholesale_catalog: list[Barrel]):
         current_ml = sum(ml_arr)
 
         selling_large = any(item.sku.startswith("LARGE") for item in wholesale_catalog)
-        threshold = .15 * ml_capacity 
+        threshold = .01 * ml_capacity 
         gold_dec = False
 
         plan = []
