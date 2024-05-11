8cad5c64fe426e826a2c7e2eeafcaa9739fef952
eswenke <swenke.ethan.us@gmail.com>
2024-05-10 17:10:05 -0700

    fixed stupid ass easkdjfhalksjdfhas error

--- src/api/catalog.py
+++ src/api/catalog.py
@@ -38,10 +38,10 @@ def get_catalog():
             )
         ).fetchall()
 
-        # firesale_pots = firesale()
-        # if len(firesale_pots) != 0:
-        #     for i in range(len(firesale_pots)):
-        #         result.append(firesale_pots[i])
+        firesale_pots = firesale()
+        if len(firesale_pots) != 0:
+            for i in range(len(firesale_pots)):
+                result.append(firesale_pots[i])
 
         res_tuple = tuple([row.sku for row in result])
 
@@ -153,7 +153,7 @@ def firesale():
             )
         ).fetchall()
 
-        if result == None:
+        if result[0][0] == None:
             return ()
         
         pot_list = tuple(result[0][0])
